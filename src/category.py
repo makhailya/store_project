10ff623d3291191079c7913c7daef811202ec662
makhailya <makhailya@gmail.com>
2025-10-19 20:35:25 +0500

    category.py

--- src/category.py
+++ src/category.py
@@ -1,31 +1,33 @@
-"""
-Модуль с описанием класса Category.
-"""
-
+from typing import List
 from src.product import Product
 
 
 class Category:
-    """Класс, описывающий категорию товаров."""
+    """Класс для категорий товаров"""
 
-    category_count = 0 #счетчик
-    product_count = 0  #счетчик
+    category_count = 0
+    product_count = 0
 
-    def __init__(self, name: str, description: str, products: list[Product]):
-        """
-        Инициализация категории.
-
-        :param name: Название категории
-        :param description: Описание категории
-        :param products: Список товаров категории
-        """
+    def __init__(self, name: str, description: str, products: List[Product] = None):
         self.name = name
         self.description = description
-        self.products = products
-
-        # Увеличиваем счётчики при создании новой категории
+        self.__products = products or []  # приватный список
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
 
-    def __repr__(self):
-        return f"<Category {self.name}: {len(self.products)} товаров>"
+    def add_product(self, product: Product) -> None:
+        """Добавляет товар в категорию"""
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError("Можно добавить только объект класса Product")
+
+    @property
+    def products(self) -> str:
+        """Возвращает список товаров в удобном для вывода виде"""
+        if not self.__products:
+            return "Нет товаров"
+        return "\n".join(
+            f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт." for p in self.__products
+        )
