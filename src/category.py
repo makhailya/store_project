6536e9fcb12cb33f3a6bd3664040391e07e64eea
makhailya <makhailya@gmail.com>
2025-10-27 00:16:56 +0500

    category.py

--- src/category.py
+++ src/category.py
@@ -8,12 +8,14 @@ class Category:
     category_count = 0
     product_count = 0
 
+
     def __init__(self, name: str, description: str, products: List[Product] = None):
         self.name = name
         self.description = description
         self.__products = products or []
         Category.category_count += 1
         Category.product_count += len(self.__products)
+
 
     def add_product(self, product: Product) -> None:
         """Добавление продукта в категорию."""
@@ -25,19 +27,21 @@ class Category:
 
     @property
     def products(self) -> List[Product]:
-        """Возвращает список объектов товаров."""
+        """Возвращает список продуктов."""
         return self.__products
 
     @property
     def products_str(self) -> str:
-        """Возвращает строку со всеми товарами (для вывода пользователю)."""
-        if not self.__products:
-            return "Нет товаров"
-        return "\n".join(
-            f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт."
-            for p in self.__products
-        ) + "\n"
+        """Возвращает строковое представление всех товаров."""
+        return "\n".join(str(p) for p in self.__products) + "\n"
+
 
     def __contains__(self, item: str) -> bool:
-        """Позволяет проверять наличие продукта по имени через 'in'."""
+        """Позволяет проверять наличие продукта по имени."""
         return any(product.name == item for product in self.__products)
+
+
+    def __str__(self) -> str:
+        """Строковое представление категории."""
+        total_quantity = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
