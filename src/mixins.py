43bd19b607cec72fa50faa2d8556e0cca3847a25
makhailya <makhailya@gmail.com>
2025-11-07 18:44:51 +0500

    added __repr__

--- src/mixins.py
+++ src/mixins.py
@@ -1,7 +1,13 @@
 class CreationLoggerMixin:
-    """Миксин, логирующий создание объектов."""
+    """Миксин, логирующий создание объектов и предоставляющий repr."""
 
     def __init__(self, *args, **kwargs):
         class_name = self.__class__.__name__
         print(f"{class_name} создан с параметрами: {args}, {kwargs}")
         super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        """Возвращает строковое представление объекта."""
+        class_name = self.__class__.__name__
+        attrs = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
+        return f"{class_name}({attrs})"
